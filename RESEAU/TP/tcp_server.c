1adb76cf23c778f40830cf70dfbf54552d932f0d
Romain Dupont <romain.d77320@gmail.com>
2025-03-16 12:02:17 +0100

    TP reseau

--- RESEAU/TP/tcp_server.c
+++ RESEAU/TP/tcp_server.c
@@ -1,26 +1,20 @@
-#ifndef TCPserver
-#define TCPserver
-
 #include <arpa/inet.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#include "tcp_server.h"
 
-int main() {
-  int server_fd, new_socket;
+int startTCPServer(const char *serverIP, int serverPort, char *buffer,
+                   int bufferSize) {
+  int server_fd, client_socket;
   struct sockaddr_in address;
   int opt = 1;
   int addrlen = sizeof(address);
-  char buffer[BUFFER_SIZE] = {0};
 
   // Create socket file descriptor
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
     perror("Socket failed");
-    exit(EXIT_FAILURE);
+    return -1;
   }
 
   // Set socket options
@@ -28,53 +22,43 @@ int main() {
                  sizeof(opt))) {
     perror("Setsockopt failed");
     close(server_fd);
-    exit(EXIT_FAILURE);
+    return -2;
   }
 
   // Define address structure
   address.sin_family = AF_INET;
-  address.sin_addr.s_addr = inet_addr("192.168.68.1"); // Bind to server's IP
-  address.sin_port = htons(PORT);
+  address.sin_addr.s_addr = inet_addr(serverIP); // Bind to server's IP
+  address.sin_port = htons(serverPort);
 
   // Bind socket to the specified port
   if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
     perror("Bind failed");
     close(server_fd);
-    exit(EXIT_FAILURE);
+    return -3;
   }
 
   // Listen for connections
   if (listen(server_fd, 3) < 0) {
     perror("Listen failed");
     close(server_fd);
-    exit(EXIT_FAILURE);
+    return -4;
   }
-
-  printf("Server listening on 192.168.68.1:%d...\n", PORT);
 
   // Accept a connection
-  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                           (socklen_t *)&addrlen)) < 0) {
+  if ((client_socket = accept(server_fd, (struct sockaddr *)&address,
+                              (socklen_t *)&addrlen)) < 0) {
     perror("Accept failed");
     close(server_fd);
-    exit(EXIT_FAILURE);
+    return -5;
   }
 
-  printf("Connection established with client.\n");
-
   // Read data from client
-  read(new_socket, buffer, BUFFER_SIZE);
+  read(client_socket, buffer, bufferSize);
   printf("Client: %s\n", buffer);
 
-  // Echo message back to client
-  send(new_socket, buffer, strlen(buffer), 0);
-  printf("Message echoed back to client.\n");
-
   // Close sockets
-  close(new_socket);
+  close(client_socket);
   close(server_fd);
 
   return 0;
 }
-
-#endif /* ifndef  TCPserver */
