0aa82cfaafb4bc74bb2824f24bca26a87463db4d
Romain Dupont <romain.d77320@gmail.com>
2025-03-19 15:56:37 +0100

    Deplacement projet reseau

--- RESEAU/TP/file_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILE_UTILS_H
-#define FILE_UTILS_H
-
-char *readFileToBuffer(const char *filePath);
-
-#endif // FILE_UTILS_H
--- RESEAU/TP/tcp_client.h
+++
@@ -1,6 +0,0 @@
-#ifndef TCP_SETUP
-#define TCP_SETUP
-
-int sendDataToServer(const char *serverIP, int serverPort, const char *data);
-
-#endif // !TCP_SETUP
