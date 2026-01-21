957667442c57ede5c778f7c68b1f892e9ba91577
camsi4 <camsi4@secil16.secil.univ-tlse3.fr>
2026-01-21 10:58:09 +0100

    Otawa done

--- M2/GCAS/WCET/TP/labwork2/CMakeFiles/otawa-dist.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/otawa-dist"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/otawa-dist.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
